57e93dbf9fc90a17158d07a3a29ccb57e8821624
Romis <romis@nesmelov.com>
2024-06-09 12:24:47 +0300

    fixed layout

--- resources/views/blocks/navbar_block.blade.php
+++ resources/views/blocks/navbar_block.blade.php
@@ -1,6 +1,6 @@
 <nav id="{{ $id }}" class="navbar navbar-expand-lg">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#{{ $id }}-bar" aria-controls="{{ $id }}-bar" aria-expanded="false" aria-label="Toggle navigation">
-        <i class="icon-menu7"></i>
+        <i class="icon-menu7 m-0"></i>
     </button>
     <div class="collapse navbar-collapse" id="{{ $id }}-bar">
         <ul class="navbar-nav">
